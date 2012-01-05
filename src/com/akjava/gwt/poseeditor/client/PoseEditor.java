1752b0a7b35a54925baa3c049564177be099eebf
aki <aki@xps>
2012-01-05 18:55:58 +0900

    initial work

---
+++ src/com/akjava/gwt/poseeditor/client/PoseEditor.java
@@ -0,0 +1,540 @@
+package com.akjava.gwt.poseeditor.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.akjava.bvh.client.AnimationBoneConverter;
+import com.akjava.bvh.client.AnimationDataConverter;
+import com.akjava.bvh.client.BVH;
+import com.akjava.bvh.client.BVHParser;
+import com.akjava.bvh.client.BVHParser.ParserListener;
+import com.akjava.gwt.html5.client.HTML5InputRange;
+import com.akjava.gwt.html5.client.HTML5InputRange.HTML5InputRangeListener;
+import com.akjava.gwt.html5.client.extra.HTML5Builder;
+import com.akjava.gwt.three.client.THREE;
+import com.akjava.gwt.three.client.core.Geometry;
+import com.akjava.gwt.three.client.core.Matrix4;
+import com.akjava.gwt.three.client.core.Object3D;
+import com.akjava.gwt.three.client.core.Vector3;
+import com.akjava.gwt.three.client.core.Vector4;
+import com.akjava.gwt.three.client.core.Vertex;
+import com.akjava.gwt.three.client.extras.GeometryUtils;
+import com.akjava.gwt.three.client.extras.ImageUtils;
+import com.akjava.gwt.three.client.extras.loaders.JSONLoader;
+import com.akjava.gwt.three.client.extras.loaders.JSONLoader.LoadHandler;
+import com.akjava.gwt.three.client.gwt.GWTGeometryUtils;
+import com.akjava.gwt.three.client.gwt.SimpleDemoEntryPoint;
+import com.akjava.gwt.three.client.gwt.animation.AnimationBone;
+import com.akjava.gwt.three.client.gwt.animation.AnimationData;
+import com.akjava.gwt.three.client.gwt.animation.AnimationHierarchyItem;
+import com.akjava.gwt.three.client.gwt.animation.AnimationKey;
+import com.akjava.gwt.three.client.gwt.animation.WeightBuilder;
+import com.akjava.gwt.three.client.lights.Light;
+import com.akjava.gwt.three.client.materials.Material;
+import com.akjava.gwt.three.client.objects.Mesh;
+import com.akjava.gwt.three.client.renderers.WebGLRenderer;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Panel;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class PoseEditor extends SimpleDemoEntryPoint{
+	private BVH bvh;
+	protected JsArray<AnimationBone> bones;
+	private AnimationData animationData;
+	@Override
+	protected void beforeUpdate(WebGLRenderer renderer) {
+		if(root!=null){
+			root.setPosition(positionXRange.getValue(), positionYRange.getValue(), positionZRange.getValue());
+			
+			root.getRotation().set(Math.toRadians(rotationRange.getValue()),Math.toRadians(rotationYRange.getValue()),Math.toRadians(rotationZRange.getValue()));
+			}
+	}
+
+	@Override
+	protected void initializeOthers(WebGLRenderer renderer) {
+		scene.add(THREE.AmbientLight(0xffffff));
+		
+		Light pointLight = THREE.DirectionalLight(0xffffff,1);
+		pointLight.setPosition(0, 10, 300);
+		scene.add(pointLight);
+		
+		Light pointLight2 = THREE.DirectionalLight(0xffffff,1);//for fix back side dark problem
+		pointLight2.setPosition(0, 10, -300);
+		scene.add(pointLight2);
+		
+		root=THREE.Object3D();
+		scene.add(root);
+		
+		loadBVH("14_01.bvh");
+	}
+
+	@Override
+	public void onMouseClick(ClickEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMouseMove(MouseMoveEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	private HTML5InputRange positionXRange;
+	private HTML5InputRange positionYRange;
+	private HTML5InputRange positionZRange;
+	private HTML5InputRange frameRange;
+	
+	private HTML5InputRange rotationRange;
+	private HTML5InputRange rotationYRange;
+	private HTML5InputRange rotationZRange;
+	@Override
+	public void createControl(Panel parent) {
+HorizontalPanel h1=new HorizontalPanel();
+		
+		rotationRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("X-Rotate:", rotationRange));
+		parent.add(h1);
+		h1.add(rotationRange);
+		Button reset=new Button("Reset");
+		reset.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationRange.setValue(0);
+			}
+		});
+		h1.add(reset);
+		
+		HorizontalPanel h2=new HorizontalPanel();
+		
+		rotationYRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("Y-Rotate:", rotationYRange));
+		parent.add(h2);
+		h2.add(rotationYRange);
+		Button reset2=new Button("Reset");
+		reset2.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationYRange.setValue(0);
+			}
+		});
+		h2.add(reset2);
+		
+		
+		HorizontalPanel h3=new HorizontalPanel();
+		rotationZRange = new HTML5InputRange(-180,180,0);
+		parent.add(HTML5Builder.createRangeLabel("Z-Rotate:", rotationZRange));
+		parent.add(h3);
+		h3.add(rotationZRange);
+		Button reset3=new Button("Reset");
+		reset3.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				rotationZRange.setValue(0);
+			}
+		});
+		h3.add(reset3);
+		
+		HorizontalPanel h4=new HorizontalPanel();
+		positionXRange = new HTML5InputRange(-50,50,0);
+		parent.add(HTML5Builder.createRangeLabel("X-Position:", positionXRange));
+		parent.add(h4);
+		h4.add(positionXRange);
+		Button reset4=new Button("Reset");
+		reset4.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				positionXRange.setValue(0);
+			}
+		});
+		h4.add(reset4);
+		
+		HorizontalPanel h5=new HorizontalPanel();
+		positionYRange = new HTML5InputRange(-50,50,0);
+		parent.add(HTML5Builder.createRangeLabel("Y-Position:", positionYRange));
+		parent.add(h5);
+		h5.add(positionYRange);
+		Button reset5=new Button("Reset");
+		reset5.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				positionYRange.setValue(0);
+			}
+		});
+		h5.add(reset5);
+		
+		HorizontalPanel h6=new HorizontalPanel();
+		positionZRange = new HTML5InputRange(-50,50,0);
+		parent.add(HTML5Builder.createRangeLabel("Z-Position:", positionZRange));
+		parent.add(h6);
+		h6.add(positionZRange);
+		Button reset6=new Button("Reset");
+		reset6.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				positionZRange.setValue(0);
+			}
+		});
+		h6.add(reset6);
+		
+		transparentCheck = new CheckBox();
+		parent.add(transparentCheck);
+		transparentCheck.setText("transparent");
+		transparentCheck.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				updateMaterial();
+			}
+		});
+		basicMaterialCheck = new CheckBox();
+		parent.add(basicMaterialCheck);
+		basicMaterialCheck.setText("BasicMaterial");
+		basicMaterialCheck.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				updateMaterial();
+			}
+		});
+		
+		HorizontalPanel frames=new HorizontalPanel();
+		frameRange = new HTML5InputRange(0,1,0);
+		parent.add(HTML5Builder.createRangeLabel("Frame:", frameRange));
+		parent.add(frames);
+		frames.add(frameRange);
+		
+		frameRange.addListener(new HTML5InputRangeListener() {
+			
+			@Override
+			public void changed(int newValue) {
+				doPose(frameRange.getValue());
+			}
+		});
+	}
+	
+	protected void updateMaterial() {
+		
+		Material material=null;
+		boolean transparent=transparentCheck.getValue();
+		double opacity=1;
+		if(transparent){
+			opacity=0.75;
+		}
+		if(basicMaterialCheck.getValue()){
+			material=THREE.MeshBasicMaterial().map(ImageUtils.loadTexture("men3smart_texture.png")).transparent(transparent).opacity(opacity).build();
+			
+		}else{
+			material=THREE.MeshLambertMaterial().map(ImageUtils.loadTexture("men3smart_texture.png")).transparent(transparent).opacity(opacity).build();
+			
+		}
+		bodyMesh.setMaterial(material);
+	}
+
+	private void loadBVH(String path){
+		
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(path));
+
+			try {
+				builder.sendRequest(null, new RequestCallback() {
+					
+					@Override
+					public void onResponseReceived(Request request, Response response) {
+						String bvhText=response.getText();
+						parseBVH(bvhText);
+
+					}
+					
+					
+					
+
+@Override
+public void onError(Request request, Throwable exception) {
+				Window.alert("load faild:");
+}
+				});
+			} catch (RequestException e) {
+				log(e.getMessage());
+				e.printStackTrace();
+			}
+	}
+
+
+	private Geometry baseGeometry;
+	protected void parseBVH(String bvhText) {
+		final BVHParser parser=new BVHParser();
+		
+		parser.parseAsync(bvhText, new ParserListener() {
+
+			@Override
+			public void onFaild(String message) {
+				log(message);
+			}
+			@Override
+			public void onSuccess(BVH bv) {
+				bvh=bv;
+				
+				AnimationBoneConverter converter=new AnimationBoneConverter();
+				bones = converter.convertJsonBone(bvh);
+				
+				AnimationDataConverter dataConverter=new AnimationDataConverter();
+				dataConverter.setSkipFirst(false);
+				animationData = dataConverter.convertJsonAnimation(bvh);
+				frameRange.setMax(animationData.getHierarchy().get(0).getKeys().length());
+				
+				JSONLoader loader=THREE.JSONLoader();
+				loader.load("men3tmp.js", new  LoadHandler() {
+					@Override
+					public void loaded(Geometry geometry) {
+						baseGeometry=geometry;
+						doPose(3651);
+					}
+				});
+			}
+		});	
+	}
+	
+	public static class MatrixAndVector3{
+		public MatrixAndVector3(){}
+		private Vector3 position;
+		public Vector3 getPosition() {
+			return position;
+		}
+		public void setPosition(Vector3 position) {
+			this.position = position;
+		}
+		private Vector3 absolutePosition;
+		
+		public Vector3 getAbsolutePosition() {
+			return absolutePosition;
+		}
+		public void setAbsolutePosition(Vector3 absolutePosition) {
+			this.absolutePosition = absolutePosition;
+		}
+		public Matrix4 getMatrix() {
+			return matrix;
+		}
+		public void setMatrix(Matrix4 matrix) {
+			this.matrix = matrix;
+		}
+		private Matrix4 matrix;
+	}
+	private List<MatrixAndVector3> boneMatrix;
+	
+	
+	/*
+	private Vector3 calculateBonedPos(Vector3 pos,AnimationBone bone,int animationIndex){
+		
+	}
+	*/
+	
+	public static List<MatrixAndVector3> boneToBoneMatrix(JsArray<AnimationBone> bones,AnimationData animationData,int index){
+		
+		List<MatrixAndVector3> boneMatrix=new ArrayList<MatrixAndVector3>();
+		//analyze bone matrix
+		for(int i=0;i<bones.length();i++){
+			AnimationBone bone=bones.get(i);
+			AnimationHierarchyItem item=animationData.getHierarchy().get(i);
+			AnimationKey motion=item.getKeys().get(index);
+			
+			//log(bone.getName());
+			
+			Matrix4 mx=THREE.Matrix4();
+			Vector3 motionPos=AnimationBone.jsArrayToVector3(motion.getPos());
+			//seems same as bone
+		//	LogUtils.log(motionPos);
+			mx.setTranslation(motionPos.getX(), motionPos.getY(), motionPos.getZ());
+			Matrix4 mx2=THREE.Matrix4();
+			mx2.setRotationFromQuaternion(motion.getRot());
+			mx.multiplySelf(mx2);
+			
+			
+			Vector3 tmpRot=THREE.Vector3();
+			tmpRot.setRotationFromMatrix(mx);
+			Vector3 tmpPos=THREE.Vector3();
+			tmpPos.setPositionFromMatrix(mx);
+			
+			//LogUtils.log(tmpPos.getX()+","+tmpPos.getY()+","+tmpPos.getZ());
+			//LogUtils.log(Math.toDegrees(tmpRot.))
+			
+			MatrixAndVector3 mv=new MatrixAndVector3();
+			Vector3 bpos=AnimationBone.jsArrayToVector3(bone.getPos());
+			
+			mv.setPosition(bpos);//not effected self matrix
+			mv.setMatrix(mx);
+			
+			if(bone.getParent()!=-1){
+				MatrixAndVector3 parentMv=boneMatrix.get(bone.getParent());
+				Vector3 apos=bpos.clone();
+				apos.addSelf(parentMv.getAbsolutePosition());
+				mv.setAbsolutePosition(apos);
+			}else{
+				//root
+				mv.setAbsolutePosition(bpos.clone());
+			}
+			boneMatrix.add(mv);
+		}
+		return boneMatrix;
+	}
+	
+	private List<List<Integer>> bonePath;
+	
+	public static  List<List<Integer>> boneToPath(JsArray<AnimationBone> bones){
+		List<List<Integer>> data=new ArrayList<List<Integer>>();
+		for(int i=0;i<bones.length();i++){
+			List<Integer> path=new ArrayList<Integer>();
+			AnimationBone bone=bones.get(i);
+			path.add(i);
+			data.add(path);
+			while(bone.getParent()!=-1){
+				//path.add(bone.getParent());
+				path.add(0,bone.getParent());
+				bone=bones.get(bone.getParent());
+			}
+		}
+		return data;
+	}
+	private JsArray<Vector4> bodyIndices;
+	private JsArray<Vector4> bodyWeight;
+	Mesh bodyMesh;
+	Object3D root;
+	Object3D bone3D;
+	private CheckBox transparentCheck;
+	private CheckBox basicMaterialCheck;
+	private void doPose(int index){
+		
+		
+		if(bodyMesh==null){//initial
+			bodyIndices = (JsArray<Vector4>) JsArray.createArray();
+			bodyWeight = (JsArray<Vector4>) JsArray.createArray();
+			WeightBuilder.autoWeight(baseGeometry, bones, 1, bodyIndices, bodyWeight);
+			}else{
+				root.remove(bodyMesh);
+			}
+		
+		
+		bonePath=boneToPath(bones);
+		boneMatrix=boneToBoneMatrix(bones,animationData,index);
+		
+		if(bone3D!=null){
+			root.remove(bone3D);
+		}
+		bone3D=THREE.Object3D();
+		root.add(bone3D);
+		
+		List<Matrix4> moveMatrix=new ArrayList<Matrix4>(); 
+		List<Vector3> parentPos=new ArrayList<Vector3>();
+		for(int i=0;i<bones.length();i++){
+			MatrixAndVector3 mv=boneMatrix.get(i);
+			Mesh mesh=THREE.Mesh(THREE.CubeGeometry(.2, .2, .2),THREE.MeshLambertMaterial().color(0xff0000).build());
+			bone3D.add(mesh);
+			
+			Vector3 pos=mv.getPosition().clone();
+			List<Integer> path=bonePath.get(i);
+			//log(bones.get(i).getName());
+			
+			Matrix4 matrix=THREE.Matrix4();
+			for(int j=0;j<path.size()-1;j++){//last is boneself
+			//	log(""+path.get(j));
+				Matrix4 mx=boneMatrix.get(path.get(j)).getMatrix();
+				matrix.multiply(matrix, mx);
+			}
+			matrix.multiplyVector3(pos);
+			matrix.multiply(matrix, boneMatrix.get(path.get(path.size()-1)).getMatrix());//last one
+			moveMatrix.add(matrix);
+			
+			
+			
+			if(bones.get(i).getParent()!=-1){
+			Vector3 ppos=parentPos.get(bones.get(i).getParent());	
+			//pos.addSelf(ppos);
+			
+			Mesh line=GWTGeometryUtils.createLineMesh(pos, ppos, 0xffffff);
+			bone3D.add(line);
+			}else{
+				//root action
+				Matrix4 mx=boneMatrix.get(0).getMatrix();
+				mx.multiplyVector3(pos);
+			}
+			mesh.setPosition(pos);
+			parentPos.add(pos);
+		}
+		
+		//Geometry geo=GeometryUtils.clone(baseGeometry);
+		
+
+		
+		
+		Geometry geo=GeometryUtils.clone(baseGeometry);
+		
+		
+		for(int i=0;i<baseGeometry.vertices().length();i++){
+			Vertex baseVertex=baseGeometry.vertices().get(i);
+			Vertex targetVertex=geo.vertices().get(i);
+			
+			int boneIndex1=(int) bodyIndices.get(i).getX();
+			int boneIndex2=(int) bodyIndices.get(i).getY();
+			
+			Vector3 bonePos=boneMatrix.get(boneIndex1).getAbsolutePosition();
+			Vector3 relatePos=bonePos.clone();
+			relatePos.sub(baseVertex.getPosition(),bonePos);
+			moveMatrix.get(boneIndex1).multiplyVector3(relatePos);
+			//relatePos.addSelf(bonePos);
+			if(boneIndex2!=boneIndex1){
+				Vector3 bonePos2=boneMatrix.get(boneIndex2).getAbsolutePosition();
+				Vector3 relatePos2=bonePos2.clone();
+				relatePos2.sub(baseVertex.getPosition(),bonePos2);
+				moveMatrix.get(boneIndex2).multiplyVector3(relatePos2);
+				
+				
+				//scalar weight
+				
+				relatePos.multiplyScalar(bodyWeight.get(i).getX());
+				relatePos2.multiplyScalar(bodyWeight.get(i).getY());
+				relatePos.addSelf(relatePos2);
+				
+				
+				/*
+				Vector3 diff=THREE.Vector3();
+				diff.sub(relatePos2, relatePos);
+				diff.multiplyScalar(bodyWeight.get(i).getY());
+				relatePos.addSelf(diff);
+				*/
+			}
+			
+			targetVertex.getPosition().set(relatePos.getX(), relatePos.getY(), relatePos.getZ());
+		}
+		//geo.setDynamic(true);
+		//geo.setDirtyVertices(true);
+		geo.computeFaceNormals();
+		geo.computeVertexNormals();
+		//geo.computeTangents();
+		Material material=THREE.MeshLambertMaterial().map(ImageUtils.loadTexture("men3smart_texture.png")).build();
+		bodyMesh=THREE.Mesh(geo, material);
+		updateMaterial();
+		root.add(bodyMesh);
+		
+		/*
+		geo.setDynamic(true);
+		geo.setDirtyVertices(true);
+		geo.computeFaceNormals();
+		geo.computeVertexNormals();
+		geo.computeTangents();
+		*/
+		}	
+}
